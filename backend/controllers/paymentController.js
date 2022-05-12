1a477190345ffc57cb5f3404602c8e9c334557df
unknown <muthumini.devtrio@gmail.com>
2022-05-12 09:31:19 +0530

    backend controllers commited

---
+++ backend/controllers/paymentController.js
@@ -0,0 +1,38 @@
+const catchAsyncErrors = require('../middleware/catchAsyncErrors');
+const KEY = process.env.STRIPE_SECRET_KEY;
+const stripe = require('stripe')(KEY);
+exports.processPayment = catchAsyncErrors(async (req, res, next) => {
+  stripe.charges.create(
+    {
+      source: req.body.tokenId,
+      amount: req.body.amount,
+      currency: 'usd',
+    },
+    (stripeErr, stripeRes) => {
+      if (stripeErr) {
+        res.status(500).json(stripeErr);
+      } else {
+        res.status(200).json(stripeRes);
+      }
+    }
+  );
+});
+// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+// exports.processPayment = catchAsyncErrors(async (req, res, next) => {
+//   const myPayment = await stripe.paymentIntents.create({
+//     amount: req.body.amount,
+//     currency: "inr",
+//     metadata: {
+//       company: "Stylish",
+//     },
+//   });
+
+//   res
+//     .status(200)
+//     .json({ success: true, client_secret: myPayment.client_secret });
+// });
+
+// exports.sendStripeApiKey = catchAsyncErrors(async (req, res, next) => {
+//   res.status(200).json({ stripeApiKey: process.env.STRIPE_API_KEY });
+// });
